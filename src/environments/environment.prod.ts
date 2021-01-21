0c51418b089bde6a0b5c5b2c69cebdb64ba2c634
fauzan <fourzan_1995@yahoo.com>
2021-01-21 16:10:33 +0800

    Html to CSS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAX0rGC66R6OUF-E_-PJdVRWGk5801vtYs",
+    authDomain: "fauzanportfolio.firebaseapp.com",
+    databaseURL: "https://fauzanportfolio-default-rtdb.firebaseio.com",
+    projectId: "fauzanportfolio",
+    storageBucket: "fauzanportfolio.appspot.com",
+    messagingSenderId: "544921501155",
+    appId: "1:544921501155:web:ca23e20be7d595ac1af4d6",
+    measurementId: "G-8FSMEZF05P"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAX0rGC66R6OUF-E_-PJdVRWGk5801vtYs",
+    authDomain: "fauzanportfolio.firebaseapp.com",
+    databaseURL: "https://fauzanportfolio-default-rtdb.firebaseio.com",
+    projectId: "fauzanportfolio",
+    storageBucket: "fauzanportfolio.appspot.com",
+    messagingSenderId: "544921501155",
+    appId: "1:544921501155:web:ca23e20be7d595ac1af4d6",
+    measurementId: "G-8FSMEZF05P"
+  }
 };
 
 /*
